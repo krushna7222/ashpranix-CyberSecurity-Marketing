1cff24ca3859a27e37bf2656dafc8beba48b8f88
Prashik Sabale <blackdarkstar71@gmail.com>
2025-11-21 11:36:20 +0530

    data

--- src/data/trainerData.ts
+++ src/data/trainerData.ts
@@ -19,7 +19,7 @@ export const trainerData: TrainerData = {
   name: "Pravin Nagare",
   title: "Senior Cybersecurity Expert & Lead Instructor",
   description:
-    "Pravin Nagare is a highly respected cybersecurity trainer and industry expert with over a decade of experience in Ethical Hacking, Cyber Forensics, SOC, and Penetration Testing. He has trained more than 15,000+ students, helping them build strong careers in top Indian and international companies.Pravin’s practical, real-world teaching style makes complex cybersecurity concepts easy to understand for beginners and working professionals alike. He has also conducted corporate workshops for professionals from leading enterprises, empowering teams to strengthen their organization’s security posture.His mission is to create India’s strongest generation of cybersecurity professionals through hands-on labs, real attack simulations, and career-oriented mentorship.",
+    "Pravin Nagare is a Cybersecurity Trainer with 10+ years of experience, specializing in Ethical Hacking, SOC, Forensics, and Penetration Testing. He has trained 15,000+ students with a practical, career-focused approach to learning.",
   experience: "10+ Years Exp",
   image: trainerimg,
 
